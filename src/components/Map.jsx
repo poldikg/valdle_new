7c7f205cf5ba2ac69d45f70d1c341ac0b66c6313
Ivan <ivan.georgiev2002k@gmail.com>
2024-05-06 16:52:55 +0200

    Map Component saved data in localStorage, not being restarted on reMount

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,42 +3,48 @@ import { useState, useEffect, useRef } from "react";
 
 const Map = () => {
     const [allMaps, setAllMaps] = useState();
-    const isMounted = useRef(false);
+    const [isMounted, setIsMounted] = useState(false);
     const [userGuess, setUserGuess] = useState("");
     const [allUserGuesses, setAllUserGuesses] = useState([]);
-    const [randomIndexMap, setRandomIndexMap] = useState(5);
+    const [randomIndexMap, setRandomIndexMap] = useState();
     const date = new Date();
     const [dateState, setDateState] = useState(date);
     
-    // console.log(allMaps, randomIndexMap)
     console.log(randomIndexMap, allMaps, isMounted)
 
+    
+
+    const testVar = testFunction();
+    console.log(testVar)
+
     useEffect(() => {
+            const getRandomIndex = localStorage.getItem("mapIndex")
+
             fetch("https://valorant-api.com/v1/maps")
             .then(res => res.json())
-            .then(allDataObj => setAllMaps(prevData => {
-                return prevData = allDataObj.data.map(map => map.displayName)
-            }))
+            .then(allDataObj => {
+                const allMaps = allDataObj.data.map(map => map.displayName)
+                setAllMaps(allMaps)
+                if(getRandomIndex) {
+                    setRandomIndexMap(parseInt(getRandomIndex))
+                }
+                else {
+                const randomIndex = Math.floor(Math.random() * allMaps.length)
+                localStorage.setItem("mapIndex", randomIndex);
+                setRandomIndexMap(randomIndex)
+                }
+            })
     }, [])
 
 
+    
 
-   useEffect(() => {
-        if(isMounted.current){
-    const randomIndex = Math.floor(Math.random() * allMaps.length)
-            setRandomIndexMap(randomIndex);
-        }
-        else{
-            isMounted.current = true;
-        }
-   }, [allMaps])
    // Ako izpolzvam isMounted v dependacy array shte vidi che ima promqna i shte runne useEffecta, no nqma da causene re-render. 
    // Zaradi tova izpozlvam state var.
 
-  
+
    const handleSubmit = (event) => {
-        event.preventDefault();
-        const map = allMaps[randomIndexMap];
+        event.preventDefault();  
         setAllUserGuesses(prevState => {
             return [...prevState, userGuess]
         })
@@ -53,7 +59,7 @@ const Map = () => {
    : <h1 style={{color:"red"}}> {guess.charAt(0).toUpperCase() + guess.slice(1)} </h1>})
    //Map ima return zashtoto kato otvorq {} zapochva callback funkciq vmesto da dobavq neshta v nov array moga da izpozlvam () i sled tova {} = ({}) vmesto return
 
-
+   
 
    
     return (
