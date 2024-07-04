526e145fa1464ebc362d8f158be10559bba392be
Ivan <ivan.georgiev2002k@gmail.com>
2024-07-04 14:10:23 +0300

    Adding suggestions for the skins, checking if user input matches any of the suggestions.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,7 +6,7 @@ const Navigation = (props) => {
 
     console.log(props)
     const [dayPassed, setDayPassed] = useState(false);
-    const removeTextDecoration = {textDecoration: "none"};
+    const removeTextDecoration = { textDecoration: "none" };
     const removeFromLocalStorage = (name) => {
         const getPreviousTime = localStorage.getItem(`${name}TaskCreated`);
         const timeNow = new Date();
@@ -15,21 +15,23 @@ const Navigation = (props) => {
         const milisecondsPerHour = 60 * 60 * 1000;
         const hourDifference = (timeNow - mapPreviosulyCreated) / milisecondsPerHour;
         console.log(hourDifference)
-        if(hourDifference >= 24){
+        if (hourDifference >= 24) {
             localStorage.removeItem(`${name}Index`)
             localStorage.removeItem(`${name}TaskCreated`)
             localStorage.removeItem(`userGuessed${name}Correctly`)
             localStorage.removeItem(`allUser${name}Guesses`)
             localStorage.removeItem(`userMade${name}Guess`)
+            localStorage.removeItem(`${name}QuoteIndex`)
+            localStorage.removeItem(`${name}AbilityIndex`)
             setDayPassed(true)
         }
     }
-    const navMenu = props.navigationData.map((item, index) => <Link style={removeTextDecoration} to={`/${item.name}`}> <div onClick={() => removeFromLocalStorage(item.name)} key={index} className="nav-item"> 
-    <img   className={`nav-img image-${item.name}`} src={`/agent-abilities/${item.img}`} alt=""/> 
-    <div  className="text-container"> <h2>{item.name}</h2> <p>{item.description}</p></div> 
+    const navMenu = props.navigationData.map((item, index) => <Link style={removeTextDecoration} to={`/${item.name}`}> <div onClick={() => removeFromLocalStorage(item.name)} key={index} className="nav-item">
+        <img className={`nav-img image-${item.name}`} src={`/agent-abilities/${item.img}`} alt="" />
+        <div className="text-container"> <h2>{item.name}</h2> <p>{item.description}</p></div>
     </div> </Link>)
 
-    return(
+    return (
         <div className="nav-container">
             {/* <li> <Link to="/Map"> Map </Link></li> */}
             {navMenu}
--- src/components/Skin.jsx
+++ src/components/Skin.jsx
@@ -7,16 +7,51 @@ const Skin = () => {
     const [allSkins, setAllSkins] = useState([]);
     const [userGuessSkin, setUserGuessSkin] = useState("");
     const [allUserGuessesSkin, setAllUserGuessesSkin] = useState([]);
+    const [weaponIndex, setWeaponIndex] = useState();
+    const [skinIndex, setSkinIndex] = useState();
+    const [skinSuggestions, setSkinSuggestions] = useState([]);
+    console.log(userGuessSkin)
 
-    console.log(allSkins)
+
     useEffect(() => {
+
+
+
+        if (userGuessSkin.length >= 1) {
+            const testArr = [];
+            for (let i = 0; i < allSkins.length; i++) {
+                if (allSkins[i].weaponName.includes(userGuessSkin.charAt(0).toUpperCase() + userGuessSkin.slice(1))) {
+                    for (let n = 0; n < allSkins[i].weaponSkins.length; n++) {
+                        testArr.push(allSkins[i].weaponSkins[n].skinName)
+                    }
+                }
+                else if (!allSkins[i].weaponName.includes(userGuessSkin.charAt(0).toUpperCase() + userGuessSkin.slice(1))) {
+                    for (let j = 0; j < allSkins[i].weaponSkins.length; j++) {
+                        if (allSkins[i].weaponSkins[j].skinName.includes(userGuessSkin.charAt(0).toUpperCase() + userGuessSkin.slice(1))) {
+                            testArr.push(allSkins[i].weaponSkins[j].skinName)
+                        }
+                    }
+                }
+            }
+            setSkinSuggestions(testArr)
+        }
+        else if (userGuessSkin === "") {
+            setSkinSuggestions([])
+        }
+
+    }, [userGuessSkin])
+
+    useEffect(() => {
+        const getWeaponIndex = localStorage.getItem("SkinWeaponIndex") ? localStorage.getItem("SkinWeaponIndex") : undefined;
+        const getSkinIndex = localStorage.getItem("SkinSkinIndex") ? localStorage.getItem("SkinSkinIndex") : undefined;
+
         fetch("https://valorant-api.com/v1/weapons")
             .then(res => res.json())
             .then(data => {
-                const sortedSkins = data.data.map(skin => {
+                const sortedSkins = data.data.map(weapon => {
                     return {
-                        weaponName: skin.displayName,
-                        weaponSkins: skin.skins.map(info => {
+                        weaponName: weapon.displayName,
+                        weaponSkins: weapon.skins.map(info => {
                             return {
                                 skinName: info.displayName,
                                 skinURL: info.displayIcon
@@ -24,24 +59,66 @@ const Skin = () => {
                         })
                     }
                 })
-                setAllSkins(sortedSkins)
+                setAllSkins(sortedSkins);
+
+                if (getWeaponIndex === undefined && getSkinIndex === undefined) {
+                    const randomWeaponIndex = Math.floor(Math.random() * sortedSkins.length);
+                    setWeaponIndex(randomWeaponIndex);
+                    localStorage.setItem("SkinWeaponIndex", randomWeaponIndex);
+
+                    const randomSkinIndex = Math.floor(Math.random() * sortedSkins[randomWeaponIndex].weaponSkins.length)
+                    setSkinIndex(randomSkinIndex);
+                    localStorage.setItem("SkinSkinIndex", randomSkinIndex);
+                }
+                else if (getWeaponIndex && getSkinIndex) {
+                    setWeaponIndex(parseInt(getWeaponIndex));
+                    setSkinIndex(parseInt(getSkinIndex));
+                }
+
+
             }
             )
     }, [])
+
+    const saveUserSkinGuess = (event) => {
+        const upperCaseUserSkinGuess = event.target.value.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
+        setUserGuessSkin(upperCaseUserSkinGuess);
+    }
+
+    const handleSubmitSkin = (event) => {
+        event.preventDefault();
+
+        if (userGuessSkin) {
+            for (let i = 0; i < skinSuggestions.length; i++) {
+                if (!skinSuggestions.includes(userGuessSkin.charAt(0).toUpperCase() + userGuessSkin.slice(1))) {
+                    console.log("false")
+                    return false;
+                }
+                else {
+                    console.log("true")
+                    return true;
+                }
+            }
+        }
+
+
+
+    }
 
     return (
         <div className='skin-page'>
             <div className='skin-section'>
                 <h1>Guess the skin</h1>
                 <div className='skin-page-image-container'>
-                    {allSkins.length > 0 && <img src={allSkins[2].weaponSkins[1].skinURL} alt="" srcset="" />}
+                    {allSkins.length > 0 && <img src={allSkins[weaponIndex].weaponSkins[skinIndex].skinURL} alt="Picture of a skin." />}
                 </div>
-                <form action="">
+                <form onSubmit={handleSubmitSkin}>
                     <div className='skin-page-inputs'>
-                        <input type="text" name="" id="" placeholder='Type a skin guess...' />
+                        <input type="text" name="" id="" placeholder='Type a skin guess...' onChange={() => saveUserSkinGuess(event)} />
                         <button> {" > "}</button>
                     </div>
                 </form>
+                {skinSuggestions}
             </div>
         </div>
     )
